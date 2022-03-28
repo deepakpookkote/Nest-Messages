32ec51334d4a24c6b7cd59a1db59a7b39d691bda
Deepak Pookkote <deepak@Deepaks-MacBook-Pro.local>
2022-03-28 19:34:02 +0530

    feat(messges): validation pipe added
    Intial validation setup added
    added string validation for post api
    added new packages
    updated package json

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,34 +1,29 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
-@Controller('/messages')
+@Controller('messages')
 export class MessagesController {
-  @Get()
-  listMessages() {
-    return [
-      {
-        id: 1,
-        text: 'Hello World',
-      },
-      {
-        id: 2,
-        text: 'Hello Universe',
-      },
-    ];
-  }
+    @Get()
+    listMessages() {
+        return [
+            {
+                id: 1,
+                text: 'Hello World'
+            },
+            {
+                id: 2,
+                text: 'Hello Universe'
+            }
+        ];
+    }
 
-  @Post()
-  createMessage() {
-    return {
-      id: 3,
-      text: 'Hello Nest',
-    };
-  }
+    @Post()
+    createMessage(@Body() body: CreateMessageDto) {
+        console.log(body);
+    }
 
-  @Get('/:id')
-  getMessage(id: number) {
-    return {
-      id,
-      text: 'Hello World',
-    };
-  }
+    @Get('/:id')
+    getMessage(@Param('id') id: string) {
+        console.log(id);
+    }
 }
